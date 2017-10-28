6b81f4d5f54adbe5d63cdbcc1b8a02e8d8b88c34
Mathieu <mathieu@woorank.com>
2017-10-28 09:49:21 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is obi.</Text>
+        <Text>Chat with people in you vinicinity.</Text>
+        <Text>No account, no signup.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
